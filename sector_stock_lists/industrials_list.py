1e78ab033a4046893ad5c4ed68a446e7448f1c4f
Billy <billy.bishop@rice.edu>
2021-08-06 14:16:40 -0500

    added correct top 5 industrial stocks by mkt cap

--- sector_stock_lists/industrials_list.py
+++ sector_stock_lists/industrials_list.py
@@ -5,14 +5,24 @@
 This file calculates the top 5 stocks in the the industrials list by marketcap
 
 """
-import yfinance as yf
+# Import modules
+import csv
+from pathlib import Path
+import pandas as pd
 
-def top_5_industrial_stocks(industrials_sp500, sp500_df_wo_index, industrials_list):
-    # list all stocks in the industrials sector of the SP500 
-    industrials_sp500 = sp500_df_wo_index.loc["Industrials"]
-    industrials_list = industrials_sp500["Symbol"].values.tolist()
+def top_5_industrial_stocks_by_marketcap(sp500_w_marketcap, industrials, industrials_list):
+    # list top stocks in the industrials sector of the SP500 BY MARKET CAP!
+    sp500_w_marketcap = pd.read_csv(
+        Path("../Resources/stock_industry_marketcap.csv")
+        )
+    sp500_w_marketcap = sp500_w_marketcap.set_index("GICS Sector")
+    industrials = sp500_w_marketcap.loc["Industrials"]
+    industrials_list = industrials.set_index("Market_Cap").sort_values(by="Market_Cap").iloc[0:5]
+    return industrials_list
         
-def market_cap(industrials_list):
+
+# Still working on second function.......
+def run(industrials_list):
 
     market_cap = {}
 
@@ -25,4 +35,6 @@ def market_cap(industrials_list):
     # then we use .sort_values to sort by the "first column" [0] and sort in decending order
     # then we only call the top 5 by using "[0:5]"
     # on average this takes 320 seconds (5 minutes 20 seconds) to run per sector
-    return pd.DataFrame(market_cap, index=[0]).T.sort_values(by=[0], ascending=False)[0:5]
+    # return pd.DataFrame(market_cap, index=[0]).T.sort_values(by=[0], ascending=False)[0:5]
+    return market_cap
+

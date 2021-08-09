4944b2171588eb2998c74ef03d10c983bc164b0e
Scott Slusher <scottslusher22@gmail.com>
2021-08-09 11:39:42 -0500

    created test.py file with modular code

---
+++ test.py
@@ -0,0 +1,88 @@
+import numpy as np
+# import yahoo finance to pull stock and crypto data from
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.optimize as optimization
+# bringing in these libraries in order to use a dynamic date selection - see sub_years
+import datetime as dt
+from datetime import date
+from workflow.MCForecastTools import MCSimulation
+from workflow.Markowitz_Model import (
+    download_data,
+    calculate_log_return,
+    generate_portfolios,
+    optimize_portfolio,
+    print_optimal_portfolio_dataframe,
+    show_optimal_portfolio,
+    show_data,
+    calculate_return,
+    clean_df_monte_carlo,
+    monte_carlo,
+    mc_line_plot,
+    mc_dist_plot
+)
+
+from app import (
+    sectors,
+    sector_interest
+)
+
+
+if __name__ == '__main__':
+    sectors_1, sectors_2, sectors_3 = sectors()
+
+    sector_interest(sectors_1, sectors_2, sectors_3)
+  # value as initial investment
+    # this is a parameter passed from questionary
+    investment = 20000
+
+    # these are variables selected by user and passed to the "download_data()" function
+    stocks  = ['AAPL', 'AMZN', 'GOOG', 'TSLA'] #stock_generator(sectors_1, sectors_2, sectors_3)
+
+    # download_data() returns a dataframe called "dataset" that will be passed to the following functions:
+    # calculate_log_return(), show_data(), calculate_return(), clean_df_monte_carlo()
+    dataset = download_data(stocks)
+    
+    # calculate_log_return() takes the dataset variable and creates the log returns to the following functions:
+    # generate_portfolio(), optimize_portfolio(), print_optimal_portfolio_dataframe(), show_optimal_portfolio()
+    # calculate_log_return() is used to NORMALIZE the dataset
+    log_daily_returns = calculate_log_return(dataset)
+
+    # generate_portfolio() takes the log_daily_returns variable created by the calculate_log_return() function
+    # from this function it will create the following lists as variables to be passed to further functions:
+    # Variables : portfolio_weights, means, risk
+    # passed to optimize_portfolio(), show_optimal_portfolio()
+    portfolio_weights, means, risks = generate_portfolios(stocks, log_daily_returns)
+
+    # optimize_portfolio() takes the portfolio_weights variable from generate_portfolios() 
+    # and the log_daily_returns from calculate_log_return()
+    # this function will return a variable named "optimum" which are the optimum weights for the selected stocks
+    # this variable will be passed to the following down range functions:
+    # show_optimal_portfolio(), monte_carlo()
+    optimum = optimize_portfolio(stocks, portfolio_weights, log_daily_returns)
+
+    # this function prints the metrics and weights of the portfolio for better clarity
+    print_optimal_portfolio_dataframe(stocks, optimum, log_daily_returns)
+
+    # calculate_return() take the dataset as a parameter to calculate the standard returns to pass to the cleaned_df_monte_carlo()
+    daily_return = calculate_return(dataset)
+
+    # clean_df_monte_carlo() takes 2 dataframes (dataset, daily_returns) and combines them together to be passed to the monte_carlo()
+    clean_df_monte_carlo = clean_df_monte_carlo(dataset, daily_return)
+
+    # monte_carlo() runs the simulation to project the 95% confidence level of the value of the portfolio based on weight allocations
+    # it returns a variable of MC_Stocks to pass to the plot functions down range
+    MC_Stocks = monte_carlo(clean_df_monte_carlo, optimum, investment)
+
+    # this function graphs the simulation to generate the optimum weights and places a red star on the weights selected
+    print(show_optimal_portfolio(stocks, optimum, log_daily_returns, means, risks))
+
+    # this function returns a graph of historical price action based on 10 years of data
+    print(show_data(dataset))
+
+    # this plots the monte_carlo() simulation
+    print(mc_line_plot(MC_Stocks))
+
+    # this plots the 95% confidence levels of the monte_carlo()
+    print(mc_dist_plot(MC_Stocks))
